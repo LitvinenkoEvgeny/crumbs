34af78606e00f55c4aad284b9e3924f55e1572f0
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2017-12-02 23:06:26 +0300

    add 0.0.0.0 to allowed_hosts

--- crumb2/settings.py
+++ crumb2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!y)1&ny6i5b+wx0d$vlzzwd+6)6%whx2_mg8@qomv=fympo8w_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 INTERNAL_IPS = ['127.0.0.1']
 
 
