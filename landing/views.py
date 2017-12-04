600e1ce106f0b3a67b5777be4193023c8686beb9
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2017-12-04 22:32:45 +0300

    if not mainInfo in db

--- landing/views.py
+++ landing/views.py
@@ -6,7 +6,7 @@ from .models import Item, MainInfo
 
 def index(request):
     items = Item.objects.all()
-    company_info = MainInfo.objects.all()[0]
+    company_info = MainInfo.objects.all()[0] if len(MainInfo.objects.all()) else []
     from_order = request.session.pop('from_order', False)
     return render(request, 'landing/index.html',
                   {'items': items, 'company_info': company_info, 'show_message': from_order})
